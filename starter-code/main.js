667fcfdb4b8c5c1b1fe79e8052ea1ac71591dc8c
Paul Williams <paul@pdwilliams.net>
2016-08-21 19:21:39 +1000

    Layout refinements

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,18 +7,18 @@ var cardFour = "king";
 
 // compare card values.
 if (cardTwo === cardFour) {
-    alert("You found a match!");
+  alert("You found a match!");
 } else {
-        alert("Sorry, try again.");
+  alert("Sorry, try again.");
 };
 */
 
 var gameBoard = document.getElementById('game-board');
 var createBoard = function () {
-    for (i = 0; i < 4; i++) {
-        var card = document.createElement('div');
-        card.className = ('card');
-        gameBoard.appendChild(card);
-    }
-}
+  for (i = 0; i < 4; i++) {
+    var card = document.createElement('div');
+    card.className = ('card');
+    gameBoard.appendChild(card);
+  };
+};
 createBoard();
