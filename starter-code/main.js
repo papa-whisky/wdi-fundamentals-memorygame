f2fa0d19e15cdfdbdfbed7d810b70aa85a52debb
Paul Williams <paul@pdwilliams.net>
2016-08-23 00:07:36 +1000

    Fixed 'clear' function + refactoring

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,62 +1,28 @@
 //Define global variables.
-var cards = ["queen", "queen", "king", "king"];
+var cardTypes = ["queen", "queen", "king", "king", "queen", "queen", "king", "king"];
 var cardsInPlay = [];
 var gameBoard = document.getElementById('game-board');
+var cards = document.getElementsByClassName('card')
 
 //Generate '.card' divs on page load.
 var createBoard = function () {
-  for (var i = 0; i < cards.length; i++) {
+  for (var i = 0; i < cardTypes.length; i++) {
     var card = document.createElement('div');
     card.className = ('card');
-    //Record type of card for later use with isMatch function.
-    card.setAttribute('data-card', cards[i]);
+    card.setAttribute('data-card', cardTypes[i]);
+    card.setAttribute('data-matched', "N");
     gameBoard.appendChild(card);
     card.addEventListener('click', clickCard);
   };
 };
 createBoard();
 
-//Compare two cards and display message.
-var isMatch = function(x) {
-  if (x[0] === x[1]) {
-    alert("You found a match!");
-    //Check whether game is complete and display message if so.
-    if (isComplete() === true) {
-      alert("Game complete!");
-    }
-  } else {
-    alert("Sorry, try again.");
-    //Clear image from cards if not matched.
-    clearCards();
-  };
-  cardsInPlay = [];
-};
-
-//Check whether all cards have been turned over (i.e. game is complete).
-var isComplete = function() {
-  var card = document.getElementsByClassName('card');
-  for (var i = 0; i < card.length; i++) {
-    if (card[i].innerHTML === "") {
-      return false;
-    };
-  };
-  return true;
-};
-
-//Function to clear images from cards.
-var clearCards = function() {
-  var card = document.getElementsByClassName('card');
-  for (var i = 0; i < card.length; i++) {
-    card[i].innerHTML = "";
-  };
-};
-
 //Actions to take when card is clicked.
 function clickCard() {
   //Check whether card has already been turned over.
   if (this.innerHTML === "") {
-    //Add card type to array for later comparison.
-    cardsInPlay.push(this.getAttribute('data-card'));
+    //Add card to array for later comparison.
+    cardsInPlay.push(this);
     //Display image on clicked card.
     if (this.getAttribute('data-card') === "queen") {
       this.innerHTML = '<img src="queen.png" alt="Queen">';
@@ -65,7 +31,44 @@ function clickCard() {
     };
     ////Call isMatch function once two cards have been clicked. Function delayed to allow image time to load. (Why does this require quotes around isMatch?)
     if (cardsInPlay.length === 2) {
-      setTimeout('isMatch(cardsInPlay)', 500);
+      setTimeout('isMatch(cardsInPlay)', 200);
+    };
+  };
+};
+
+//Compare two cards and display message.
+var isMatch = function(x) {
+  if (x[0].getAttribute('data-card') === x[1].getAttribute('data-card')) {
+    alert("You found a match!");
+    //Mark cards as matched.
+    x[0].setAttribute('data-matched', "Y");
+    x[1].setAttribute('data-matched', "Y");
+    //Check whether game is complete and display message if so.
+    if (isComplete() === true) {
+      alert("Game complete!");
+    };
+  } else {
+    alert("Sorry, try again.");
+    clearCards();
+  };
+  cardsInPlay = [];
+};
+
+//Check whether all cards have been turned over (i.e. game is complete).
+var isComplete = function() {
+  for (var i = 0; i < cards.length; i++) {
+    if (cards[i].getAttribute('data-matched') === "N") {
+      return false;
+    };
+  };
+  return true;
+};
+
+//Clear images from unmatched cards.
+var clearCards = function() {
+  for (var i = 0; i < cards.length; i++) {
+    if (cards[i].getAttribute('data-matched') === "N") {
+      cards[i].innerHTML = "";
     };
   };
 };
