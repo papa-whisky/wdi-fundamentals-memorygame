6194f9ef6ae221746d452443b61849b9cb3fd9fb
Paul Williams <paul@pdwilliams.net>
2016-08-31 01:18:26 +1000

    v2.0

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Memory Game: Unit Homework for WDI Fundamentals
 
 In order to apply JavaScript concepts covered in the [pre-work](http://fundamentals.generalassemb.ly/), all pre-work lessons will work towards building a memory game app. Your development of this game will exercise your ability to implement logic with JavaScript and so much more! Besides following the prompts, you should feel free to be creative and add to the code - make this game your own!
 
+********
+v2.0
+********
+-Attempted mobile-first, responsive design
+-Gave game a theme of vintage cars
+-Altered game to match images to descriptions rather than images to images
+
--- starter-code/main.js
+++ starter-code/main.js
@@ -1,92 +1,126 @@
-//Define global variables.
-var cardTypes = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
-var cardsInPlay = [];
-var gameBoard = document.getElementById('game-board');
-var cards = document.getElementsByClassName('card')
-var startButton = document.getElementById('start');
-var numInput = document.getElementById('number');
+//Global variables.
+var cardValues = [
+  "911R (1967)",
+  "917K (1970)",
+  "906 Carrera 6 (1966)",
+  "Abarth 356B Carrera GTL (1960)",
+  "550 Spyder (1953)",
+  "935-77 (1977)",
+  "718 RSK Spyder (1959)",
+  "904 Carrera GTS (1964)",
+  "911 Carrera RSR (1973)",
+  "911 Carrera RS 2.7 (1973)",
+  "356A Speedster (1956)",
+  "804 (1962)"];
 var deck = [];
-var instButton = document.getElementById('instButton');
+var cardsInPlay = [];
+var numOfPairs = document.getElementsByClassName('pairs');
+var beginButton = document.getElementsByClassName('begin');
+var aboutButton = document.getElementsByTagName('h3');
+var aboutText = document.getElementsByTagName('p');
+var gameBoard = document.getElementsByClassName('game-board');
+var cards = document.getElementsByClassName('card');
 
-instButton.addEventListener("click", function () {
-  if (document.getElementById('arrow').innerHTML === '˅') {
-    document.getElementById('text').className = ('show');
-    document.getElementById('arrow').innerHTML = '˄';
+//Display or hide 'about' text.
+aboutButton[0].addEventListener("click", function () {
+  if (aboutButton[0].innerHTML === 'ABOUT ↓') {
+    aboutText[0].className = 'show';
+    aboutButton[0].innerHTML = 'ABOUT ↑';
   } else {
-    document.getElementById('text').className = ('');
-    document.getElementById('arrow').innerHTML = '˅';
+    aboutText[0].className = ('');
+    aboutButton[0].innerHTML = 'ABOUT ↓';
+  };
+});
+
+//Handler for pressing 'enter' from input field.
+numOfPairs[0].addEventListener("keyup", function (event) {
+  if (event.keyCode == 13) {
+    beginButton[0].click();
   };
 });
 
 //Check that user input is valid, start game if so.
 var checkInput = function () {
-  if (parseInt(numInput.value) % 2 === 0 && parseInt(numInput.value) > 3) {
-    createBoard(parseInt(numInput.value));
-    numInput.value = "";
-    numInput.className = "";
-    numInput.setAttribute('placeholder', 'Number of cards');
-    startButton.value = "Restart Game";
+  if (numOfPairs[0].value > 0) {
+    createBoard(numOfPairs[0].value);
+    numOfPairs[0].value = "";
+    numOfPairs[0].className = "pairs";
+    numOfPairs[0].setAttribute('placeholder', 'Enter number of pairs');
+    beginButton[0].value = "RESTART";
   } else {
-    numInput.className = ('invalid');
-    numInput.value = "";
-    numInput.setAttribute('placeholder', 'Enter an even number above 3');
+    numOfPairs[0].className += ' invalid';
+    numOfPairs[0].value = "";
+    numOfPairs[0].setAttribute('placeholder', 'Enter a number above 0');
   };
 };
-
-//Handler for pressing enter from textbox.
-numInput.addEventListener("keyup", function (event) {
-  if (event.keyCode == 13) {
-    startButton.click();
-  };
-});
 
 //Generate deck, shuffle, create game board.
 var createBoard = function (x) {
   clearBoard();
-  //Create half of deck randomly.
-  for (var i = 0; i < x / 2; i++) {
-    var card = document.createElement('div');
-    var rand = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-    card.className = ('card');
-    card.setAttribute('data-card', rand);
-    card.setAttribute('data-matched', "N");
-    deck.push(card);
-    card.addEventListener('click', clickCard);
-  };
-  //Duplicate first half of deck (so every card will have a pair).
-  for (var i = 0; i < x / 2; i++) {
-    var card = document.createElement('div');
-    card.className = ('card');
-    card.setAttribute('data-card', deck[i].getAttribute('data-card'));
-    card.setAttribute('data-matched', "N");
-    deck.push(card);
-    card.addEventListener('click', clickCard);
+  //Create deck randomly but in pairs (half of cards with image type, half with text).
+  for (var i = 0; i < x; i++) {
+    var card1 = document.createElement('div');
+    var card2 = document.createElement('div');
+    var card1Content = document.createElement('div');
+    var card2Content = document.createElement('div');
+    var rand = cardValues[Math.floor(Math.random() * cardValues.length)];
+    card1.className = ('card');
+    card1.setAttribute('data-value', rand);
+    card1.setAttribute('data-type', 'img');
+    card1.setAttribute('data-matched', "N");
+    deck.push(card1);
+    card1.addEventListener('click', clickCard);
+    card1.appendChild(card1Content);
+    card1Content.className = ('content');
+    card2.className = ('card');
+    card2.setAttribute('data-value', rand);
+    card2.setAttribute('data-type', 'text');
+    card2.setAttribute('data-matched', "N");
+    deck.push(card2);
+    card2.addEventListener('click', clickCard);
+    card2.appendChild(card2Content);
+    card2Content.className = ('content vcenter');
   };
   //Shuffle deck.
   shuffleArray(deck);
   //Create game board.
-  for (var i = 0; i < x; i++) {
-    gameBoard.appendChild(deck[i]);
+  for (var i = 0; i < x * 2; i++) {
+    gameBoard[0].appendChild(deck[i]);
   };
 };
 
 //Clear game board.
 var clearBoard = function () {
   deck = [];
-  document.getElementById('win').className = ('');
-  while (gameBoard.firstChild) {
-    gameBoard.removeChild(gameBoard.firstChild);
+  document.getElementById('win').className = ('banner');
+  while (gameBoard[0].firstChild) {
+    gameBoard[0].removeChild(gameBoard[0].firstChild);
   };
+};
+
+//Durstenfeld shuffle algorithm.
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  };
+  return array;
 };
 
 //Actions to take when card is clicked.
 function clickCard() {
   //Check whether card has already been turned over.
-  if (this.innerHTML === "" && cardsInPlay.length < 2) {
+  if (this.firstChild.innerHTML === "" && cardsInPlay.length < 2) {
     //Add card to array for later comparison.
     cardsInPlay.push(this);
-    //Display image on clicked card.
-    this.innerHTML = '<img src="images/' + this.getAttribute('data-card') + '.png" alt="' + this.getAttribute('data-card') + '">';
+    //Display image or text on clicked card.
+    if (this.getAttribute('data-type') === "img") {
+      this.firstChild.innerHTML = '<img src="images/' + this.getAttribute('data-value') + '.jpg" alt="' + this.getAttribute('data-value') + '">';
+    } else {
+      this.firstChild.innerHTML = this.getAttribute('data-value');
+    }
     //Once two cards have been clicked, call function to compare. Delay to allow image time to load. (**Why does function need to be in quotes?**)
     if (cardsInPlay.length === 2) {
       setTimeout('isMatch(cardsInPlay)', 100);
@@ -96,7 +130,7 @@ function clickCard() {
 
 //Compare two cards and display message.
 var isMatch = function (x) {
-  if (x[0].getAttribute('data-card') === x[1].getAttribute('data-card')) {
+  if ((x[0].getAttribute('data-value') === x[1].getAttribute('data-value')) && (x[0].getAttribute('data-type') !== x[1].getAttribute('data-type'))) {
     //Mark cards as matched.
     for (var i = 0; i < 2; i++) {
       x[i].setAttribute('data-matched', "Y");
@@ -105,7 +139,7 @@ var isMatch = function (x) {
     cardsInPlay = [];
     //Check whether game is complete and display message if so.
     if (isComplete() === true) {
-      document.getElementById('win').className = ('show');
+      document.getElementById('win').className += (' show');
       document.getElementById('win').scrollIntoView();
     };
   } else {
@@ -130,19 +164,8 @@ var isComplete = function () {
 var clearCards = function () {
   for (var i = 0; i < cards.length; i++) {
     if (cards[i].getAttribute('data-matched') === "N") {
-      cards[i].innerHTML = "";
+      cards[i].firstChild.innerHTML = "";
       cards[i].className = ('card');
     };
   };
-};
-
-//Durstenfeld shuffle algorithm.
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  };
-  return array;
 };
